87cef122310a592139fd9a10160a530a2cbfd483
leven101 <leven101@gmail.com>
2011-02-13 15:45:11 +0000

    initial import

---
+++ GIZA++-v2/TTables.cpp
@@ -0,0 +1,377 @@
+/*
+
+EGYPT Toolkit for Statistical Machine Translation
+Written by Yaser Al-Onaizan, Jan Curin, Michael Jahr, Kevin Knight, John Lafferty, Dan Melamed, David Purdy, Franz Och, Noah Smith, and David Yarowsky.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+USA.
+ 
+*/
+#include "TTables.h"
+#include "Parameter.h"
+
+GLOBAL_PARAMETER(float,PROB_CUTOFF,"PROB CUTOFF","Probability cutoff threshold for lexicon probabilities",PARLEV_OPTHEUR,1e-7);
+GLOBAL_PARAMETER2(float, COUNTINCREASE_CUTOFF,"COUNTINCREASE CUTOFF","countCutoff","Counts increment cutoff threshold",PARLEV_OPTHEUR,1e-6);
+
+#ifdef BINARY_SEARCH_FOR_TTABLE
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::printCountTable(const char *filename, 
+					 const Vector<WordEntry>& evlist, 
+					 const Vector<WordEntry>& fvlist,
+					 const bool actual) const
+{
+  ofstream of(filename);
+  for(unsigned int i=0;i<lexmat.size();++i)
+    {
+      if( lexmat[i] )
+	for(unsigned int j=0;j<lexmat[i]->size();++j)
+	  {
+	    const CPPair&x=(*lexmat[i])[j].second; // x = CPPair
+	    WordIndex e=i,f=(*lexmat[i])[j].first; //e=sourceID, f=targetID
+	    if( x.prob>PROB_SMOOTH )
+	      if( actual )
+		of << evlist[e].word << ' ' << fvlist[f].word << ' ' << x.count << '\n';
+	      else
+		of << e << ' ' << f << ' ' << x.count << '\n';
+	  }
+    }
+}
+
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::printProbTable(const char *filename, 
+					 const Vector<WordEntry>& evlist, 
+					 const Vector<WordEntry>& fvlist,
+					 const bool actual) const
+{
+  ofstream of(filename);
+  for(unsigned int i=0;i<lexmat.size();++i)
+    {
+      if( lexmat[i] )
+	for(unsigned int j=0;j<lexmat[i]->size();++j)
+	  {
+	    const CPPair&x=(*lexmat[i])[j].second; // x = CPPair
+	    WordIndex e=i,f=(*lexmat[i])[j].first; //e=sourceID, f=targetID
+	    if( x.prob>PROB_SMOOTH )
+	      if( actual )
+		of << evlist[e].word << ' ' << fvlist[f].word << ' ' << x.count << ' ' << x.prob << '\n';
+	      else
+		of << e << ' ' << f << ' ' << x.count << ' ' << x.prob << '\n';
+	  }
+    }
+}
+extern float step_k;
+extern float step_alpha;
+template <class COUNT, class PROB>
+COUNT tmodel<COUNT, PROB>::interpolateStepCounts() {
+  const float neg_alpha = step_alpha * (-1);
+  const float mu_k = pow(step_k + 2, neg_alpha);  
+  const float OneMinusMu_k = 1 - mu_k;
+  typename map<wordPairIds, COUNT>::const_iterator stpItr = stepCounts_.begin();
+  for(; stpItr != stepCounts_.end(); ++stpItr) {
+    // for each wordPairIds in stepCounts_  get tTable count
+    LpPair<COUNT, PROB>* tc = getPtr(stpItr->first.first, stpItr->first.second);
+    massert(tc != 0);
+    //if(tc->count != 0)
+    if(tc->count > PROB_SMOOTH)  // get interpolated count
+      tc->count = ((OneMinusMu_k * tc->count) + (mu_k * stpItr->second));
+    else tc->count = stpItr->second;
+  }
+  stepCounts_.clear();
+  return ++step_k;
+}
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::normalizeTable(const vcbList& srcl, const vcbList& trgl, 
+  const set<WordIndex>* srcHits, bool clearCounts)
+{
+  //vector< vector<pair<unsigned int,CPPair> >* > lexmat;
+  //cerr << "Total lexmat size = " << lexmat.size() << endl;
+  //const Vector<WordEntry>& srclist(srcl.getVocabList()); 
+  //const Vector<WordEntry>& trglist(trgl.getVocabList());
+  for(unsigned int i=0;i<lexmat.size();++i) // i = iterate through all source words
+    {
+      // only bother with source words that were "touched" during this step
+      if(step_k != 0 && (srcHits->find(i) == srcHits->end())) continue;
+      double c=0.0;
+      if(lexmat[i]) 
+	{
+	  unsigned int lSize=lexmat[i]->size(); //lsize = # of possible translations for src_i 
+	  for(unsigned int j=0;j<lSize;++j)  // j = a translation of src_i
+          {
+	    c+=(*lexmat[i])[j].second.count;
+          }
+          //cerr << "total count for '" << srclist[i].word << "' = " << c << endl; 
+	  for(unsigned int j=0;j<lSize;++j)
+	    {
+	      if(c==0)
+		(*lexmat[i])[j].second.prob=1.0/(lSize);
+	      else 
+                (*lexmat[i])[j].second.prob=(*lexmat[i])[j].second.count/c;
+	      if((step_k == 0) && clearCounts) (*lexmat[i])[j].second.count=0; // counts aren't cleared for OEM
+	    }
+	}
+    }
+}
+            //cerr << "count for pair (" << srclist[i].word << "," << trglist[(*lexmat[i])[j].first].word << ") = " 
+            //  << (*lexmat[i])[j].second.count << endl;
+                //cerr << "prob for pair (" << srclist[i].word << "," << trglist[(*lexmat[i])[j].first].word << ") = " 
+                  //<<  (*lexmat[i])[j].second.count << "/" << c << " = " << (*lexmat[i])[j].second.prob << endl;
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::printProbTableInverse(const char *, 
+				   const Vector<WordEntry>&, 
+				   const Vector<WordEntry>&, 
+				   const double, 
+				   const double, 
+				   const bool ) const
+{
+}
+//implemented 10/19/2009 ADL (copied from other version below)
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::readProbTable(const char *filename){
+  ifstream inf(filename);
+  cerr << "Reading T prob. table from  " << filename << "\n";
+  if(!inf){
+    cerr << "\nERROR: Cannot open " << filename << "\n";
+    return;
+  }
+  WordIndex src_id, trg_id;
+  PROB prob;
+  COUNT count;
+  int nEntry=0;
+  while(inf >> src_id  >> trg_id >> count >> prob){
+    insert(src_id, trg_id, count, prob);
+    nEntry++;
+  }
+  cerr << "Read " << nEntry << " entries in prob. table.\n";
+}
+
+template class tmodel<COUNT,PROB> ; 
+#else
+/* ------------------ Method Definiotns for Class tmodel --------------------*/
+
+#
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::printCountTable(const char *filename, 
+					 const Vector<WordEntry>& evlist, 
+					 const Vector<WordEntry>& fvlist,
+					 const bool actual) const
+     // this function dumps the t table. Each line is of the following format:
+     //
+     // c(target_word/source_word) source_word target_word
+{
+  ofstream of(filename);
+  typename hash_map<wordPairIds, CPPair, hashpair, equal_to<wordPairIds> >::const_iterator i;
+  for(i = ef.begin(); i != ef.end();++i){
+    if ( ((*i).second).count >  COUNTINCREASE_CUTOFF)
+      if (actual)
+	of <<  ((*i).second).count << ' ' << evlist[ ((*i).first).first ].word << ' ' << fvlist[((*i).first).second].word << ' ' << (*i).second.prob << '\n';
+      else 
+	of << ((*i).second).count << ' ' <<  ((*i).first).first  << ' ' << ((*i).first).second << ' ' << (*i).second.prob << '\n';
+  }
+}
+
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::printProbTable(const char *filename, 
+					 const Vector<WordEntry>& evlist, 
+					 const Vector<WordEntry>& fvlist,
+					 const bool actual) const
+     // this function dumps the t table. Each line is of the following format:
+     //
+     // source_word target_word p(target_word/source_word)
+{
+  ofstream of(filename);
+  typename hash_map<wordPairIds, CPPair, hashpair, equal_to<wordPairIds> >::const_iterator i;
+  for(i = ef.begin(); i != ef.end();++i)
+    if( actual )
+      of << evlist[((*i).first).first].word << ' ' << 
+	fvlist[((*i).first).second].word << ' ' << (*i).second.prob << '\n';
+    else
+      of << ((*i).first).first << ' ' << ((*i).first).second << ' ' << 
+	(*i).second.prob << '\n';
+}
+
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::printProbTableInverse(const char *filename, 
+				   const Vector<WordEntry>& evlist, 
+				   const Vector<WordEntry>& fvlist, 
+				   const double, 
+				   const double, 
+				   const bool actual) const
+  // this function dumps the inverse t table. Each line is of the format:
+  //
+  // target_word_id source_word_id p(source_word/target_word)
+  //
+  // if flag "actual " is true then print actual word entries instead of 
+  // token ids
+{
+  cerr << "Dumping the t table inverse to file: " << filename << '\n';
+  ofstream of(filename);
+  typename hash_map<wordPairIds, CPPair, hashpair, equal_to<wordPairIds> >::const_iterator i;
+  PROB p_inv = 0 ;
+  //  static const PROB ratio(double(fTotal)/eTotal);
+  WordIndex e, f ;
+  int no_errors(0);
+  vector<PROB> total(fvlist.size(),PROB(0)) ; // Sum over all e of P(f/e) * p(e) - needed for normalization
+ 
+  for(i = ef.begin(); i != ef.end(); i++){
+    e = ((*i).first).first ;
+    f = ((*i).first).second ;
+    total[f] += (PROB) evlist[e].freq * ((*i).second.prob); //add P(f/ei) * F(ei) 
+  }
+  
+  for(i = ef.begin(); i != ef.end(); i++){
+    e = ((*i).first).first ;
+    f = ((*i).first).second ;
+    p_inv = ((*i).second.prob) * (PROB) evlist[e].freq / total[f] ;
+    if (p_inv > 1.0001 || p_inv < 0){
+      no_errors++;
+      if (no_errors <= 10){
+	cerr << "printProbTableInverse(): Error - P("<<evlist[e].word<<"("<<
+	  e<<") / "<<fvlist[f].word << "("<<f<<")) = " << p_inv <<'\n';
+	cerr << "f(e) = "<<evlist[e].freq << " Sum(p(f/e).f(e)) = " << total[f] <<
+	  " P(f/e) = " <<((*i).second.prob)  <<'\n';
+	if (no_errors == 10)
+	  cerr<<"printProbTableInverse(): Too many P inverse errors ..\n";
+      }
+    }
+    if (actual)
+      of << fvlist[f].word << ' ' << evlist[e].word << ' ' << p_inv << '\n';
+    else 
+      of << f << ' ' << e << ' ' << p_inv <<  '\n';
+  }
+}
+/*
+
+
+
+{
+  cerr << "Dumping the t table inverse to file: " << filename << '\n';
+  ofstream of(filename);
+  hash_map<wordPairIds, CPPair, hashpair, equal_to<wordPairIds> >::const_iterator i;
+  PROB p_inv = 0 ;
+  static const PROB ratio(double(fTotal)/eTotal);
+  WordIndex e, f ;
+  for(i = ef.begin(); i != ef.end(); i++){
+    e = ((*i).first).first ;
+    f = ((*i).first).second ;
+    p_inv = ((*i).second.prob) * ratio * (PROB) evlist[e].freq / 
+      (PROB) fvlist[f].freq ;
+    if (actual)
+      of << fvlist[f].word << ' ' << evlist[e].word << ' ' << p_inv << '\n';
+    else 
+      of << f << ' ' << e << ' ' << p_inv <<  '\n';
+  }
+}
+*/
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::normalizeTable(const vcbList&engl, const vcbList&french, int iter)
+  // normalize conditional probability P(fj/ei):
+  // i.e. make sure that Sum over all j of P(fj/e) = 1  
+  // this method reads the counts portion of the table and normalize into
+  // the probability portion. Then the counts are cleared (i.e. zeroed)
+  // if the resulting probability of an entry is below a threshold, then 
+  // remove it .
+{
+  cerr << "GETS HERE \n"; exit(1);
+  if( iter==2 )
+    {
+      total2.resize(engl.uniqTokens());for(unsigned int i=0;i<total2.size();i++)total2[i]=0.0;
+    }
+  nFrench.resize(engl.uniqTokens());for(unsigned int i=0;i<nFrench.size();i++)nFrench[i]=0;
+  nEng.resize(french.uniqTokens());for(unsigned int i=0;i<nEng.size();i++)nEng[i]=0;
+  Vector<double> total(engl.uniqTokens(),0.0);
+  //Vector<int> nFrench(engl.uniqTokens(), 0);
+  //Vector<int> nEng(french.uniqTokens(), 0);
+
+  typename hash_map<wordPairIds, CPPair, hashpair, equal_to<wordPairIds> >::const_iterator i;
+  for(i = ef.begin(); i != ef.end(); i++){ // for all possible source words e
+    if( iter==2 )
+      total2[((*i).first).first] += (*i).second.count;
+    total[((*i).first).first] += (*i).second.count;
+    nFrench[((*i).first).first]++;
+    nEng[((*i).first).second]++;
+  }
+  for(unsigned int k=0;k<engl.uniqTokens();++k)
+    if( nFrench[k] )
+      {
+	double probMass=(french.uniqTokensInCorpus()-nFrench[k])*PROB_SMOOTH;
+	if( probMass<0.0 )
+	  cout << k << " french.uniqTokensInCorpus(): " << french.uniqTokensInCorpus() << "  nFrench[k]:"<< nFrench[k] << '\n';
+	total[k]+= total[k]*probMass/(1-probMass);
+      }
+  typename hash_map<wordPairIds, CPPair, hashpair, equal_to<wordPairIds> >::iterator j, k;
+  PROB p ;
+  int nParams=0;
+  for(j = ef.begin(); j != ef.end(); ){
+    k = j;
+    k++ ;
+    if( (total[((*j).first).first])>0.0 )
+      p = ((((*j).second).count) /(total[((*j).first).first])) ;
+    else
+      p= 0.0;
+    if (p > PROB_CUTOFF)
+      {
+	if( iter>0 )
+	  {
+	    ((*j).second).prob = 0 ;
+	    ((*j).second).count = p ;
+	  }
+	else
+	  {
+	    ((*j).second).prob = p ;
+	    ((*j).second).count = 0 ;
+	  }
+	nParams++;
+      }
+    else {
+      erase(((*j).first).first, ((*j).first).second);
+    }
+    j = k ;
+  }
+  if( iter>0 )
+    return normalizeTable(engl, french, iter-1);
+  else
+    {
+    }
+}
+
+template <class COUNT, class PROB>
+void tmodel<COUNT, PROB>::readProbTable(const char *filename){
+  /* This function reads the t table from a file.
+     Each line is of the format:  source_word_id target_word_id p(target_word|source_word)
+     This is the inverse operation of the printTable function.
+     NAS, 7/11/99
+  */
+  ifstream inf(filename);
+  cerr << "Reading t prob. table from  " << filename << "\n";
+  if(!inf){
+    cerr << "\nERROR: Cannot open " << filename << "\n";
+    return;
+  }
+  WordIndex src_id, trg_id;
+  PROB prob;
+  int nEntry=0;
+  while(inf >> src_id  >> trg_id  >> prob){
+    insert(src_id, trg_id, 0.0, prob);
+    nEntry++;
+  }
+  cerr << "Read " << nEntry << " entries in prob. table.\n";
+}
+
+template class tmodel<COUNT,PROB> ; 
+
+/* ---------------- End of Method Definitions of class tmodel ---------------*/
+
+
+#endif
---
+++ bin/grow-diag.cmd
@@ -0,0 +1,1 @@
+../../bin/giza2bal.pl -d batch.Ahmm.10 -i inv.batch.Ahmm.10 | ../../bin/symal -alignment="grow" -diagonal="yes" -final="yes" -both="yes" > align.final
